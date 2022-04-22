0137ea6a547872fbc0ab40db483f4f33d47c3388
Jubilee <jubilee2@gmail.com>
2022-04-21 22:02:08 -0500

    add administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :articles
+      resources :comments
+
+      root to: "articles#index"
+    end
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
